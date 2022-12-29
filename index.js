ff4bfe9195510fe323c4228dcb1d046ae3a8178a
zahidhasan125 <zahidhasan125@gmail.com>
2022-12-29 17:47:20 +0600

    task edit and remove api added, completed task api added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -36,6 +36,60 @@ const run = async () => {
             const tasks = await taskCollections.find(query).toArray();
             res.send(tasks);
         })
+
+        app.patch('/tasks', async (req, res) => {
+            const id = req.query.id;
+            const filter = { _id: ObjectId(id) }
+            const option = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    isCompleted: true
+                }
+            }
+            const result = await taskCollections.updateOne(filter, updateDoc, option)
+            res.send(result);
+        })
+        app.delete('/tasks', async (req, res) => {
+            const id = req.query.id;
+            const query = { _id: ObjectId(id) }
+            const result = await taskCollections.deleteOne(query);
+            res.send(result)
+        })
+
+        app.patch('/edittask', async (req, res) => {
+            const id = req.query.id;
+            const name = req.query.editTask;
+            const filter = { _id: ObjectId(id) }
+            const option = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    taskName: name
+                }
+            }
+            const result = await taskCollections.updateOne(filter, updateDoc, option);
+            res.send(result);
+            console.log(id, name);
+        })
+
+        app.get('/completed', async (req, res) => {
+            const query = { isCompleted: true };
+            const completed = await taskCollections.find(query).toArray();
+            res.send(completed);
+        })
+
+        app.patch('/completed', async (req, res) => {
+            const id = req.query.id;
+            const filter = { _id: ObjectId(id) }
+            const option = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    isCompleted: false
+                }
+            }
+            const result = await taskCollections.updateOne(filter, updateDoc, option)
+            res.send(result);
+        })
+
     }
     finally {
 
